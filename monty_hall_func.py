da27289265936cfa8782c2740c0879c1f7cd53bb
Mark Lattimore <mark.d.lattimore@gmail.com>
2023-07-14 20:15:09 -0400

    Add rudimentary input validation

--- monty_hall_func.py
+++ monty_hall_func.py
@@ -2,37 +2,45 @@ from random import randint
 
 
 
+
 def play():
-    number = int(Element('number').value)
+    results = Element('results')
+
+    try:
+        number = int(Element('number').value)
+    except:
+        number = 0
+        results.element.innerText = "Enter an integer"
+
     switch = True
     win = 0
     loss = 0
-    for _ in range(number):
-        choices = [1, 2, 3]
-        car = choices.pop(randint(0, 2))
-        goats = choices
-        selection = randint(1, 3)
+    if number > 0:
+        for _ in range(number):
+            choices = [1, 2, 3]
+            car = choices.pop(randint(0, 2))
+            goats = choices
+            selection = randint(1, 3)
 
-        if selection == car:
-            if switch:
-                # print("You lose.")
-                loss += 1
-            else:
-                # print("You win!")
-                win += 1
+            if selection == car:
+                if switch:
+                    # print("You lose.")
+                    loss += 1
+                else:
+                    # print("You win!")
+                    win += 1
 
-        elif selection != car:
-            if switch:
-                # print("You win!")
-                win += 1
-            else:
-                # print("You lose!")
-                loss += 1
+            elif selection != car:
+                if switch:
+                    # print("You win!")
+                    win += 1
+                else:
+                    # print("You lose!")
+                    loss += 1
 
-    results = Element('results')
-    results.element.innerText = f"""\
-    Wins: {win}
-    Losses: {loss}
-    Win Percentage: {(win/number) * 100:.3f} %
+        results.element.innerText = f"""\
+        Wins: {win}
+        Losses: {loss}
+        Win Percentage: {(win/number) * 100:.3f} %
 
-    """
+        """
